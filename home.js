379528012f633493b5ef28660d29684d9a095d29
Jeff Seaman <46333921+jeffreyjseamanppu@users.noreply.github.com>
2021-07-26 16:56:43 -0400

    Update home.js

--- home.js
+++ home.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "Password Here",
-  database: "Database Name",
+  database: "Database Names",
   charset: 'utf8'
 });
 
